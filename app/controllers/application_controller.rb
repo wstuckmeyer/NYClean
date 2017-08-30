71e5adeb5163371a8bcbb082943e281ce1b26204
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-08-30 11:00:56 -0400

    created routes for all tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name][:avatar][:neighborhood])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
