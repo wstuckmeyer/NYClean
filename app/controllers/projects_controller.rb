7d5e162c0d559aa59e534a4a541899c69e546bd3
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-08-31 20:21:16 -0400

    fixed project cards

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
 
 	def create
 		@current_user = current_user.id
-		@projects = Project.create(name: params[:project][:name], description: params[:project][:description], date: params[:project][:date], photos: params[:project][:photo], location: params[:project][:location], people: params[:project][:people], user_id: current_user)
+		@projects = Project.create(name: params[:project][:name], description: params[:project][:description], date: params[:project][:date], photos: params[:project][:photos], location: params[:project][:location], people: params[:project][:people], user_id: current_user)
 
 		redirect_to project_path
 	end
