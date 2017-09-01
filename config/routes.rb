65c3e6ab819beca457b72ffa2606b279ff116b91
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-09-01 18:11:22 -0400

    breaking stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "home#index"
-resources :users, :projects, :boards, :messages
+resources :users, :projects, :boards, :messages, :volunteers
 end
