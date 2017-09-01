175cc4df81e728bbb7a64b3638adf3287dd1b6d0
Richard Love <ricklove@outloook.com>
2017-09-01 15:36:21 -0400

    added edit and delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+
 	def index
 		@projects = Project.all
 	end
@@ -18,21 +21,33 @@ class ProjectsController < ApplicationController
 		redirect_to @projects
 	end
 
-	# def update
-	# 	if project.update(project_params)
-	# 		redirect_to project_path(@project)
-	# end
-
-	# def destroy
-	# 	@project.destroy
-	# 	redirect_to root_path
-	# end
-
-	# private
-
-	# def project_params
-	# 	params.require(:project).permit(:name, :description, :date, :photo, :people, :user_id)
-	# end
+	def edit
+		@projects = Project.find(params[:id])
+	end
 
 
+	def update
+		@project = Project.find(params[:id])
+		if @project.update(project_params)
+			redirect_to project_path(@project)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id]).delete
+		redirect_to user_path
+	end
+
+
+	private
+
+	def project_params
+		params.require(:project).permit(:name, :description, :date, :photo, :people, :user_id)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
 end
