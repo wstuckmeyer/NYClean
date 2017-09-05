f2f8150bdd9f435335d4344d25a50e141c3249d9
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-09-05 16:01:17 -0400

    ui tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831182658) do
+ActiveRecord::Schema.define(version: 20170905183644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170831182658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "people"
+    t.time "time"
   end
 
   create_table "users", force: :cascade do |t|
